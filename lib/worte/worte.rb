b51aff0e8e9917d6e4c1e665262f539e7c7b8bed
Jonas Oberschweiber <jonas.oberschweiber@d-velop.de>
2015-10-15 18:12:22 +0200

    Pass options to aspell

--- lib/worte/worte.rb
+++ lib/worte/worte.rb
@@ -4,13 +4,14 @@ require 'ffi/aspell'
 
 module Worte
   class Worte
-    def initialize(language)
+    def initialize(language, options={})
       @language = language
+      @options = options
     end
 
     def check(str)
       tokens = Tokenizer.new.tokenize(str)
-      FFI::Aspell::Speller.open(@language) do |speller|
+      FFI::Aspell::Speller.open(@language, @options) do |speller|
         tokens.each do |token|
           token.correct = speller.correct?(token.word)
         end
