6e57871e2c38d9f3373d3022d2badde7122dcb43
Jonas Oberschweiber <jonas.oberschweiber@d-velop.de>
2015-10-15 18:45:51 +0200

    Add support for filters

--- lib/worte/worte.rb
+++ lib/worte/worte.rb
@@ -6,10 +6,18 @@ module Worte
   class Worte
     def initialize(language, options={})
       @language = language
+
+      @filters = options.delete(:filters) || []
+      single_filter = options.delete(:filter)
+      @filters << single_filter if single_filter
+
       @options = options
     end
 
     def check(str)
+      @filters.each do |filter|
+        str = filter.filter(str)
+      end
       tokens = Tokenizer.new.tokenize(str)
       FFI::Aspell::Speller.open(@language, @options) do |speller|
         tokens.each do |token|
