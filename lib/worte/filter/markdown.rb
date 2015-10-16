9401199cc8a516346d3c76355c7ff63f104b9b2c
Jonas Oberschweiber <jonas.oberschweiber@d-velop.de>
2015-10-16 18:56:11 +0200

    Improve the markdown filter and add a spec

--- lib/worte/filter/markdown.rb
+++ lib/worte/filter/markdown.rb
@@ -2,9 +2,15 @@ module Worte
   module Filter
     class Markdown
       def filter(str)
+        str = str.gsub(/^---$\n.*?\n^---$/m) { |c| "\n" * c.split("\n").length }
+        str = str.gsub(/^```[a-z]*$\n.*?\n^```$/m) { |c| "\n" * (c.split("\n").length - 1) }
+        str = str.gsub(/^\t.*$/) { |c| '' }
         str = str.gsub(/^([ \t]*[#]+)/) { |c| ' ' * c.length }
         str = str.gsub(/\*\*(.*?)\*\*/) { |c| '  ' + c[2..-3] + '  ' }
         str = str.gsub(/\*(.*?)\*/) { |c| ' ' + c[1..-2] + ' ' }
+        str = str.gsub(/\_\_(.*?)\_\_/) { |c| '  ' + c[2..-3] + '  ' }
+        str = str.gsub(/\_(.*?)\_/) { |c| ' ' + c[1..-2] + ' ' }
+        str = str.gsub(/`.*?`/) { |c| ' ' * c.length }
         str = str.gsub(/[!]?\[.*\]\(.*\)/) { |c| ' ' * c.length }
       end
     end
---
+++ spec/markdown_filter_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+describe Worte::Filter::Markdown do
+  it "filters out star emphasis" do
+    f = Worte::Filter::Markdown.new
+    t = f.filter('Test *test* test')
+    expect(t).to eq('Test  test  test')
+  end
+
+  it "filters out star bold" do
+    f = Worte::Filter::Markdown.new
+    t = f.filter('Test **test** test')
+    expect(t).to eq('Test   test   test')
+  end
+
+  it "filters out underline emphasis" do
+    f = Worte::Filter::Markdown.new
+    t = f.filter('Test _test_ test')
+    expect(t).to eq('Test  test  test')
+  end
+
+  it "filters out underline bold" do
+    f = Worte::Filter::Markdown.new
+    t = f.filter('Test __test__ test')
+    expect(t).to eq('Test   test   test')
+  end
+
+  it "filters heading marks" do
+    f = Worte::Filter::Markdown.new
+    t = f.filter('### Test')
+    expect(t).to eq('    Test')
+  end
+
+  it "filters out Jekyll property block" do
+    f = Worte::Filter::Markdown.new
+    t = f.filter(<<-EOF
+---
+title: Test test test
+category: ---test
+---
+Hello, World
+EOF
+    )
+    expect(t).to eq("\n\n\n\n\nHello, World\n")
+  end
+
+  it "filters out inline backticks" do
+    f = Worte::Filter::Markdown.new
+    t = f.filter("Test `test` Test")
+    expect(t).to eq("Test        Test")
+  end
+
+  it "filters out fenced code blocks" do
+    f = Worte::Filter::Markdown.new
+    t = f.filter("Test\n```ruby\nx = 1 + 2\nputs(x)\n```\nHello, World!")
+    expect(t).to eq("Test\n\n\n\n\nHello, World!")
+  end
+
+  it "filters out indented pre blocks" do
+    f = Worte::Filter::Markdown.new
+    t = f.filter("Test\n\tThis\n\tis\n\ta pre block\nHello, World")
+    expect(t).to eq("Test\n\n\n\nHello, World")
+  end
+end
